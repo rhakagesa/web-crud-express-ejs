14cbb1b746b6d4f1e753b50134473a296db2568a
rhakagesa <rhakagesa@gmail.com>
2024-06-05 23:41:16 +0700

    fix routes user

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,9 +7,6 @@ const authentication = require("../middlewares/authentication");
 const { userAuthorization } = require("../middlewares/authorization");
 
 router.use(authentication);
-router.get("/page/user", (req, res) => {
-    res.render("user");
-})
 router.get("/", UserController.getAllUsers);
 router.get("/info", UserController.getUser);
 router.post("/update/:id", userAuthorization, UserController.updateUser);
